9dd0fec54158f0c885eb2a9bc4e109a9bfbad359
hermansemykozov <semikozovgerman@gmail.com>
2021-06-01 16:45:47 +0300

    CHANGE: add validation rules, rewrite optional question group component

--- src/components/assignment-1/index.jsx
+++ src/components/assignment-1/index.jsx
@@ -1,18 +1,52 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import {Card} from "../card";
 import Box from "@material-ui/core/Box";
 import {Message} from "../message";
 import {makeStyles} from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
+import SubdirectoryArrowLeftIcon from "@material-ui/icons/SubdirectoryArrowLeft";
+import Button from "@material-ui/core/Button";
+import {OptionalQuestionItem} from "../optional-question-item";
+import List from "@material-ui/core/List";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControl from "@material-ui/core/FormControl";
 
 const useStyles = makeStyles((theme) => ({
   section: {
     paddingTop: theme.spacing(4),
   },
+  button: {
+    marginTop: theme.spacing(4),
+  },
 }));
 
 export const Assignment1 = ({ sectionRef, targetRef, handleClick, children }) => {
   const classes = useStyles();
+
+  const [value, setValue] = useState('');
+  const [error, setError] = useState(false);
+  const [isOpen, showAnswer] = useState(false);
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [isSubmitActive, setIsSubmitActive] = useState(true);
+
+  const handleRadioChange = (event) => {
+    setValue(event.target.value);
+    setError(false);
+    console.log("handleRadioChange")
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (value !== "") {
+      setError(false);
+      setIsSubmitted(true);
+      !error && setIsSubmitActive(false);
+      !error && showAnswer(true);
+    } else {
+      setError(true);
+    }
+  };
 
   useEffect(() => {
     sectionRef.current.scrollIntoView({
@@ -71,11 +105,172 @@ export const Assignment1 = ({ sectionRef, targetRef, handleClick, children }) =>
             With a deep sigh, you sink back into your chair. “The early bird gets the worm? Yeah, right.”
           </Typography>
         </Box>
-        <Box mt={2}>
+        <Box mt={2} mb={2}>
           <Typography variant="body1">
             You pull up the information on the course that Nathan mentioned, Wallet Detox. Which of its performance metrics are you going to look at?
           </Typography>
         </Box>
+        <form onSubmit={handleSubmit}>
+          <FormControl component="fieldset" error={error}>
+            <RadioGroup aria-label="quiz" name="quiz" value={value} onChange={handleRadioChange}>
+              <List style={{ counterReset: "alphabeticList" }}>
+                <OptionalQuestionItem
+                  isTrue={false}
+                  isOpen={isOpen}
+                  value={value}
+                  isSubmitted={isSubmitted}
+                  id={1}
+                  // error={error}
+                  text="NPS (Net Promoter Score)"
+                >
+                  <p style={{marginTop: 0}}>Partly true.</p>
+                  <p>
+                    NPS is a good choice, but it only gets you so far. It measures the satisfaction of people who have completed the course, but not the people who bought but didn’t finish it.
+                  </p>
+                  <p>Other metrics are as one-sided as NPS:</p>
+                  <ul>
+                    <li>CSAT measures customer experience.</li>
+                    <li>Completion rate measures your course’s success at prompting the audience to complete it.</li>
+                    <li>The number of signups measures how well you are marketing the course.</li>
+                    <li>Revenue measures the income generated from people buying the course.</li>
+                  </ul>
+                  <p style={{marginBottom: 0}}>
+                    The correct answer is “all of the above”. You need all metrics to decide what went right and what went wrong.
+                  </p>
+                </OptionalQuestionItem>
+                <OptionalQuestionItem
+                  isTrue={false}
+                  isOpen={isOpen}
+                  value={value}
+                  isSubmitted={isSubmitted}
+                  id={2}
+                  // error={error}
+                  text="CSAT (Customer Satisfaction Index)"
+                >
+                  <p style={{marginTop: 0}}>Partly true.</p>
+                  <p>
+                    CSAT measures the experience of the existing audience. It doesn’t tell you anything about why people decided not to buy your course or how many people completed it.
+                  </p>
+                  <p>Each metric helps paint a fuller picture:</p>
+                  <ul>
+                    <li>NPS measures customer loyalty.</li>
+                    <li>Completion rate measures your course’s success at prompting the audience to complete it.</li>
+                    <li>The number of signups measures how well you are marketing the course.</li>
+                    <li>Revenue measures the income generated from people buying the course.</li>
+                  </ul>
+                  <p style={{marginBottom: 0}}>
+                    The correct answer is “all of the above”. To understand why this course is performing as badly as it is, you need to look at it from all angles.
+                  </p>
+                </OptionalQuestionItem>
+                <OptionalQuestionItem
+                  isTrue={false}
+                  isOpen={isOpen}
+                  value={value}
+                  isSubmitted={isSubmitted}
+                  id={3}
+                  // error={error}
+                  text="Number of signups"
+                >
+                  <p style={{marginTop: 0}}>Partly true.</p>
+                  <p>
+                    The number of signups alone won’t tell you the full story. It is a good indicator of your marketing efforts, but not of the quality of your product.
+                  </p>
+                  <p>The same applies to other metrics:</p>
+                  <ul>
+                    <li>NPS only measures customer loyalty.</li>
+                    <li>CSAT measures customer experience.</li>
+                    <li>Completion rate measures your course’s success at prompting the audience to complete it.</li>
+                    <li>Revenue measures the income generated from people buying the course.</li>
+                  </ul>
+                  <p style={{marginBottom: 0}}>
+                    The correct answer is “all of the above”. There’s no be-all and end-all metric to measure the overall success of a course.
+                  </p>
+                </OptionalQuestionItem>
+                <OptionalQuestionItem
+                  isTrue={false}
+                  isOpen={isOpen}
+                  value={value}
+                  isSubmitted={isSubmitted}
+                  id={4}
+                  // error={error}
+                  text="Revenue"
+                >
+                  <p style={{marginTop: 0}}>Partly true.</p>
+                  <p>
+                    Revenue measures the income that was generated from people buying the course. It does not include potential or lost revenue from the audience you failed to reach.
+                  </p>
+                  <p>The success of a given course depends on many variables:</p>
+                  <ul>
+                    <li>Customer loyalty, which is measured by NPS.</li>
+                    <li>Customer experience, which is measured by CSAT.</li>
+                    <li>Completion rate, or your course’s success at prompting the audience to complete it.</li>
+                    <li>The number of signups, which indicates how well you are marketing the course.</li>
+                  </ul>
+                  <p style={{marginBottom: 0}}>The correct answer is “all of the above”.</p>
+                </OptionalQuestionItem>
+                <OptionalQuestionItem
+                  isTrue={false}
+                  isOpen={isOpen}
+                  value={value}
+                  isSubmitted={isSubmitted}
+                  id={5}
+                  // error={error}
+                  text="COR (Completion Rate)"
+                >
+                  <p style={{marginTop: 0}}>Partly true.</p>
+                  <p>
+                    The completion rate measures the quality of your content. The higher the completion rate, the more people found the course valuable and engaging enough to finish.
+                  </p>
+                  <p>But, there are still many pieces of the puzzle that you’re missing:</p>
+                  <ul>
+                    <li>Customer loyalty, which is measured by NPS.</li>
+                    <li>Customer experience, which is measured by CSAT.</li>
+                    <li>Revenue, which is the income generated from people who bought your course.</li>
+                    <li>The number of signups, which indicates how well you are marketing the course.</li>
+                  </ul>
+                  <p style={{marginBottom: 0}}>
+                    The correct answer is “all of the above”. Performance metrics are always used in conjunction with each other, to maximize their value.
+                  </p>
+                </OptionalQuestionItem>
+                <OptionalQuestionItem
+                  isTrue={true}
+                  isOpen={isOpen}
+                  value={value}
+                  isSubmitted={isSubmitted}
+                  id={6}
+                  // error={error}
+                  text="All of the above"
+                >
+                  <p style={{marginTop: 0}}>Ding, ding, ding!</p>
+                  <p>
+                    No one metric can paint you the whole picture. They are used for measuring the specific aspects of a product:</p>
+                  <ul>
+                    <li>NPS measures customer loyalty.</li>
+                    <li>CSAT measures customer experience.</li>
+                    <li>Completion rate measures your course’s success at prompting the audience to complete it.</li>
+                    <li>The number of signups measures how well you are marketing the course.</li>
+                    <li>Revenue measures the income generated from people buying the course.</li>
+                  </ul>
+                </OptionalQuestionItem>
+              </List>
+            </RadioGroup>
+          </FormControl>
+          <div style={{ color: "red", marginBottom: "20px", marginTop: "-10px"}}>{error && "choose option"}</div>
+          {!isSubmitted && (<Button type="submit" variant="outlined" color="primary" size="small" disabled={!isSubmitActive}>
+            Check Answer
+          </Button>)}
+        </form>
+        {isSubmitted && (<Button
+          type="button"
+          variant="contained"
+          color="primary"
+          size="small"
+          className={classes.button}
+          endIcon={<SubdirectoryArrowLeftIcon />}
+          onClick={() => handleClick(targetRef)}
+        >
+          Continue
+        </Button>)}
       </Card>
     </Box>
   )
--- src/components/optional-question-item/index.jsx
+++ src/components/optional-question-item/index.jsx
@@ -1,9 +1,11 @@
-import { Collapse, Grid, ListItem, ListItemIcon, ListItemSecondaryAction, ListItemText, Tooltip } from '@material-ui/core';
+import { Collapse, Grid, ListItem, ListItemSecondaryAction, ListItemText, Tooltip } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import { Cancel, Check, CheckCircle } from '@material-ui/icons';
-import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
+// import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
 import React from 'react';
+import Radio from "@material-ui/core/Radio";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 
 const useStyles = makeStyles((theme) => ({
   label: {
@@ -23,10 +25,15 @@ const useStyles = makeStyles((theme) => ({
     counterIncrement: "alphabeticList",
     border: "1px solid #E3E7EB",
     borderRadius: "8px",
+    paddingLeft: theme.spacing(3),
+    paddingRight: theme.spacing(3),
     marginBottom: theme.spacing(1),
     width: "auto",
     display: "flex",
     alignItems: "center",
+  },
+  listItemError: {
+    borderColor: "red",
   },
   listItemIcon: {
     minWidth: 32,
@@ -37,20 +44,25 @@ const useStyles = makeStyles((theme) => ({
       position: "absolute",
       top: 0,
       left: 0,
+      // left: "-12px",
       fontSize: "0.8rem",
-      width: "24px",
-      height: "24px",
-      lineHeight: "24px",
+      width: "42px",
+      height: "42px",
+      lineHeight: "41px",
       verticalAlign: "middle",
       textAlign: "center",
       color: "blue",
       fontWeight: 600,
     },
   },
+  formControlLabel: {
+    minWidth: 32,
+  },
   answerPanel: {
-    paddingLeft: theme.spacing(2),
+    paddingLeft: theme.spacing(1.5),
     paddingRight: theme.spacing(2),
     paddingBottom: theme.spacing(2),
+    // marginLeft: "-5px",
     display: "flex",
     flexWrap: "nowrap",
     alignItems: "flex-start",
@@ -61,32 +73,40 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export const OptionalQuestionItem = ({ isTrue = false, text, answer }) => {
+export const OptionalQuestionItem = ({ isTrue = false, isOpen, value, isSubmitted, id, error, text, children }) => {
   const classes = useStyles();
+  const labelId = `input-list-label-${id}`;
 
   return (
     <>
       <ListItem
         button={true}
-        onClick={() => console.log("clicked")}
-        className={classes.listItem}
+        className={`${classes.listItem} ${error && classes.listItemError}`}
+        disabled={isSubmitted}
       >
-        <ListItemIcon className={classes.listItemIcon}>
-          <CheckBoxOutlineBlankIcon color={isTrue ? "primary" : "action"} />
-        </ListItemIcon>
-        <ListItemText style={{ listStyleType: "none" }}>{text}</ListItemText>
+        <ListItemText id={labelId} style={{ listStyleType: "none" }}>
+          <FormControlLabel className={classes.formControlLabel} label={text} control={
+            <Radio
+              edge="start"
+              tabIndex={-1}
+              disableRipple
+              value={text}
+              color={"primary"}
+              inputProps={{ 'aria-labelledby': labelId }}
+            />
+          } />
+        </ListItemText>
         <ListItemSecondaryAction>
-          {isTrue && (
-            <Tooltip title="Remove" aria-label="Remove">
+          {isOpen && value === text && (
+            <Tooltip title="Correct" aria-label="Correct">
               <Check color="primary" />
             </Tooltip>
           )}
         </ListItemSecondaryAction>
       </ListItem>
       <Collapse
-        in={true}
+        in={isOpen}
         timeout="auto"
-        addEndListener={() => console.log("collapse")}
       >
         {/* isAnswerOpen used before */}
         <Grid container spacing={1} className={classes.answerPanel}>
@@ -102,7 +122,7 @@ export const OptionalQuestionItem = ({ isTrue = false, text, answer }) => {
             className={classes.answerPanelItem}
             style={{ flexGrow: 1 }}
           >
-            <Typography variant="body1">{answer}</Typography>
+            <Typography variant="body1">{children}</Typography>
           </Grid>
         </Grid>
       </Collapse>
