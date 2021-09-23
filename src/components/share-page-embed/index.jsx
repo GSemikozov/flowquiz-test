6325e20dded99ba7685c00c1b2228d148bca9609
hermansemykozov <semikozovgerman@gmail.com>
2021-09-23 10:54:13 +0300

    replace sharing by object via iframe

--- src/components/share-page-embed/index.jsx
+++ src/components/share-page-embed/index.jsx
@@ -51,7 +51,9 @@ const BootstrapDialogTitle = (props) => {
 export const SharePageEmbed = () => {
   const [open, setOpen] = useState(false);
 
-  const getCopyToClipboardContent = () => `<object data="${SHARED_PAGE_LINK}" width="100%" height="400"></object>`;
+  const getCopyToClipboardContent = () =>
+    `<iframe src="${SHARED_PAGE_LINK}" height="400" width="100%" style="border: none; box-shadow: none;"></iframe>`
+    // `<object data="${SHARED_PAGE_LINK}" width="100%" height="400"></object>`;
 
   const handleClickOpen = () => {
     setOpen(true);
